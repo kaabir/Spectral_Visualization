92206723e36152ce92109b1587f5a017b8638ef6
Kaabir <38142420+kaabir@users.noreply.github.com>
2019-11-21 00:17:24 +0530

    Fix Out Of Index Error

--- DAQ/Test.py
+++ DAQ/Test.py
@@ -15,13 +15,13 @@ spectreReadings = [0.0,0.0,0.0,0.0,0.0,0.0]
 x = np.linspace(450.00, 650.00, num=6, endpoint=True)
 defaultYLimit = 860
 xnew = np.linspace(450.00, 650.00, num=6, endpoint=True) 
-fig = plt.figure(1)
+fig = plt.figure()
 plt.ion()     
 
 def getMaximumIntensity(currentReadings):
-    maxIntensityIndex = 0
+    maxIntensityIndex = -1
     for num, value in enumerate(currentReadings,start=1):
-        if (value > currentReadings[maxIntensityIndex]):
+        if (maxIntensityIndex == -1 or value > currentReadings[maxIntensityIndex]):
                 maxIntensityIndex = num
     return maxIntensityIndex
         
@@ -35,25 +35,26 @@ def handle_close(evt):
 	#exit()
 	#sys.exit()
  
-def doPlot():     
-	fig.canvas.mpl_connect('close_event', handle_close) # figured out finally how to close the "groundhog" window
-	plt.ylim(0,defaultYLimit)                            
-	plt.title('Spectral Response')           
-	plt.grid(True)                              
-	plt.ylabel('Intensity Count')    
-	plt.xlabel('Wavelength in nm')                      
+def doPlot():
+    plt2 = fig.add_subplot(121)     
+    fig.canvas.mpl_connect('close_event', handle_close) # figured out finally how to close the "groundhog" window
+    plt2.set_ylim(0,defaultYLimit)                            
+    plt2.set_title('Spectral Response')           
+    plt2.grid(True)                              
+    plt2.set_ylabel('Intensity Count')    
+    plt2.set_xlabel('Wavelength in nm')                      
 	#plt.plot(spectreReadings, 'ro-', label='Spectral readings') 
-	plt.plot(x, spectreReadings, 'o', xnew, f(xnew), '-')       
-	plt.legend(loc='upper left')
+    plt2.plot(x, spectreReadings, 'o', xnew, f(xnew), '-')       
+    plt2.legend(loc='upper left')
 
 def plotWavelengthVSTime():
-    fig2 = plt.figure(2)
-    plt.plot(times, maxReadingsTillNow)
-    plt.title('Max Wavelength vs Time')
-    plt.grid = False
-    plt.ylabel('Maximum Wavelength')
-    plt.xlabel('Time (in seconds)')
-
+    plt2 = fig.add_subplot(222)
+    plt2.set_title('Max Wavelength vs Time')
+    plt2.set_ylim(0,defaultYLimit)
+    plt2.grid(False)
+    plt2.set_ylabel('Maximum Wavelength')
+    plt2.set_xlabel('Time (in seconds)')
+    plt2.plot(times, maxReadingsTillNow)
 
 times = []
 maxReadingsTillNow = []
@@ -65,25 +66,25 @@ while (loop):
 	spectreString = spectreData.readline().decode('utf8')
 	spectreList = spectreString.split(",")
 	if(len(spectreList)==6):
-        readingArrivalTime = datetime.datetime.now()
+		readingArrivalTime = datetime.datetime.now()
 		for num, value in enumerate(spectreList,start=0):
 			spectreReadings[num]=float(value)
         
 		maxIntensityIndex = getMaximumIntensity(spectreReadings)
-		print("Maximum intensity is " + str(spectreReadings[maxIntensityIndex]))
+		if(maxIntensityIndex != -1 and maxIntensityIndex < len(spectreReadings)):
+			print("Maximum intensity is " + str(spectreReadings[maxIntensityIndex]))
+			maxReadingsTillNow.append(spectreReadings[maxIntensityIndex])
+			times.append((readingArrivalTime - startTime).total_seconds())
+		else:
+			print("Maximum intensity anamoly detected ")           
         
 		defaultYLimit=max(spectreReadings)*1.1 # thus, the cubic spline should remain inside the plotarea
 		f=interp1d(x,spectreReadings,kind='cubic')
-
-        maxReadingsTillNow.append(spectreReadings[maxIntensityIndex])
-        times.append(readingArrivalTime - startTime)
-
 		drawnow(doPlot)
-        plotWavelengthVSTime()
+		#plotWavelengthVSTime()
 
-data = {'Wavelength':[x],
-        'Intensity':[spectreReadings]}        
-df = pd.DataFrame(data, columns= ['Wavelength', 'Intensity'])
-export_csv = df.to_csv (r'C:\Users\debacle\Documents\Plot\DAQ\export_dataframe.csv', index = True, header=True)
-#export_csv = df.to_csv (r'C:\Users\debacle\Documents\Plot\DAQ\export_dataframe.csv', index = None, header=True) #Don't forget to add '.csv' at the end of the path        
+#data = {'Wavelength':[x],
+ #       'Intensity':[spectreReadings]}        
+#df = pd.DataFrame(data, columns= ['Wavelength', 'Intensity'])
+#export_csv = df.to_csv (r'C:\Users\debacle\Documents\Plot\DAQ\export_dataframe.csv', index = True, header=True)
 print(".")
