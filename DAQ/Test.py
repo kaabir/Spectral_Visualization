968c6e11da8d15c55c3b7b60aa06c278dcdc4583
Kaabir <38142420+kaabir@users.noreply.github.com>
2019-12-01 01:45:41 +0530

    Add Wavelength Time Plot Support
    - Fixed Data Frame
    - Added Minor Ticks Support

--- DAQ/Test.py
+++ DAQ/Test.py
@@ -1,19 +1,32 @@
-import serial  
 import numpy   as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 from drawnow import *  
 from serial import Serial
 from scipy.interpolate import interp1d
 import pandas as pd
 import datetime
 
-spectreData = serial.Serial("COM23", 115200) 
+class MaxIntensity:
+    intensity = 0.0
+    wavelength = 0.0
+    time = 0
+    
+    def __init__(self,intensity, wavelength, time):
+        self.intensity = intensity
+        self.wavelength = wavelength
+        self.time = time
+        
+        
+#spectreData = serial.Serial("COM23", 115200) 
+spectreData =[0.0,0.0,0.0,0.0,0.0,0.0] 
 
 loop=True
-
+allReadings = []
 spectreReadings = [0.0,0.0,0.0,0.0,0.0,0.0] 
 x = np.linspace(450.00, 650.00, num=6, endpoint=True)
-defaultYLimit = 860
+defaultYLimit = 1024
+defaultWavelengthLimit = 650.00
 xnew = np.linspace(450.00, 650.00, num=6, endpoint=True) 
 fig = plt.figure()
 plt.ion()     
@@ -33,28 +46,46 @@ def handle_close(evt):
 	loop=False
 
 def plot1():
-    plt1 = fig.add_subplot(221)     
-    fig.canvas.mpl_connect('close_event', handle_close) # figured out finally how to close the "groundhog" window
-    plt1.set_ylim(0,defaultYLimit)                            
-    plt1.set_title('Spectral Response')           
-    plt1.grid(True)                              
-    plt1.set_ylabel('Intensity Count')    
-    plt1.set_xlabel('Wavelength (in nm)')                          
-    plt1.legend(loc='upper left')
-    plt1.plot(x, spectreReadings, 'o', xnew, f(xnew), '-')
+    fig.canvas.mpl_connect('close_event', handle_close)
+    ax1 = fig.add_subplot(311, )      
+    ax1.set_ylim(0,defaultYLimit)                            
+    ax1.set_title('Spectral Response')           
+    ax1.grid(True)                              
+    ax1.set_ylabel('Intensity Count')    
+    ax1.set_xlabel('Wavelength (in nm)')                          
+    ax1.legend(loc='upper left')
+    ax1.xaxis.set_minor_locator(MultipleLocator(5))
+    ax1.xaxis.set_minor_formatter(FormatStrFormatter("%d"))
+    ax1.plot(x, spectreReadings, 'r', xnew, f(xnew), '-')
     
-    plt2 = fig.add_subplot(222)
-    plt2.set_title('Max Wavelength vs Time')
-    plt2.set_ylim(0,defaultYLimit)
-    plt2.grid(False)
-    plt2.set_ylabel('Maximum Wavelength')
-    plt2.set_xlabel('Time (in seconds)')
-    plt2.plot(times, maxReadingsTillNow)
+    intensities = map(lambda x: x.intensity, maxIntensities)
+    times = map(lambda x: x.time, maxIntensities)
+    wavelengths = map(lambda x: x.wavelength, maxIntensities)
+    
+    ax2 = fig.add_subplot(312, autoscaley_on = True)
+    ax2.set_title('Max Intensity vs Time')
+    ax2.set_ylim(0,defaultYLimit)
+    ax2.grid(True)
+    ax2.set_ylabel('Maximum Intensity')
+    ax2.set_xlabel('Time (in seconds)')
+    ax2.plot(times, intensities, 'g')
+    plt.tight_layout()
+    
+    
+    ax3 = fig.add_subplot(313)
+    ax3.set_title('Max Wavelength vs Time')
+    ax3.set_ylim(0,defaultWavelengthLimit)
+    ax3.grid(True)
+    ax3.set_ylabel('Maximum Wavelength')
+    ax3.set_xlabel('Time (in seconds)')
+    ax3.plot(times, wavelengths, 'b')
+    plt.tight_layout()
 
-times = []
-maxReadingsTillNow = []
+#times = []
+#maxIntensitiesTillNow = []
+maxIntensities = []
 startTime =  datetime.datetime.now()
-while (loop):            
+while (loop):         
 	while (spectreData.inWaiting()== 0):        
 		pass            
 	spectreString = spectreData.readline().decode('utf8')
@@ -63,16 +94,39 @@ while (loop):
 		readingArrivalTime = datetime.datetime.now()
 		for num, value in enumerate(spectreList,start=0):
 			spectreReadings[num]=float(value)
-        
 		maxIntensityIndex = getMaximumIntensity(spectreReadings)
 		if(maxIntensityIndex != -1 and maxIntensityIndex < len(spectreReadings)):
 			print("Maximum intensity is " + str(spectreReadings[maxIntensityIndex]))
-			maxReadingsTillNow.append(spectreReadings[maxIntensityIndex])
-			times.append((readingArrivalTime - startTime).total_seconds())
+			#maxIntensitiesTillNow.append(spectreReadings[maxIntensityIndex])
+			allReadings.append(spectreReadings)
+			time = (readingArrivalTime - startTime).total_seconds()
+			intensity = MaxIntensity(spectreReadings[maxIntensityIndex], x[maxIntensityIndex], time)
+			maxIntensities.append(intensity)
 		else:
 			print("Maximum intensity anamoly detected ")           
         
-		defaultYLimit=max(spectreReadings)*1.1 # thus, the cubic spline should remain inside the plotarea
+		defaultYLimit=max(spectreReadings)*1.3
 		f=interp1d(x,spectreReadings,kind='cubic')
 		drawnow(plot1)
+"""
+data = {'Wavelength':[x],'Intensity':[spectreReadings],'Time':[times]}        
+df = pd.DataFrame(data, columns= ['Wavelength', 'Intensity', 'Time'])
+df1 = df.transpose()
+"""
+
+wavelength450 = map(lambda x: x[0], allReadings)
+wavelength490 = map(lambda x: x[1], allReadings)
+wavelength530 = map(lambda x: x[2], allReadings)
+wavelength570 = map(lambda x: x[3], allReadings)
+wavelength610 = map(lambda x: x[4], allReadings)
+wavelength650 = map(lambda x: x[5], allReadings)
+data = {'450':wavelength450, 
+        '490':wavelength490, 
+        '530':wavelength530,
+        '570':wavelength570,
+        '610':wavelength610, 
+        '650':wavelength650, 
+        'Time':[times]}        
+df = pd.DataFrame(data, columns= ['450', '490', '530', '570', '610', '650', 'Time'])
+export_csv = df.to_csv (r'C:\Users\debacle\Documents\Plot\DAQ\export_dataframe.csv', index = True, header=True)        
 print(".")
